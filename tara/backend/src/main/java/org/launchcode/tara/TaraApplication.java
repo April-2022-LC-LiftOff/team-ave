eb39cf34cc2b51de2ca1e421e55d1b4ec0785ddf
Kat <kathryn.garnier@gmail.com>
2022-04-21 20:41:06 -0400

    Added front and back end project files

---
+++ tara/backend/src/main/java/org/launchcode/tara/TaraApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.tara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaraApplication.class, args);
+	}
+
+}
---
+++ tara/backend/src/test/java/org/launchcode/tara/TaraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.tara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
